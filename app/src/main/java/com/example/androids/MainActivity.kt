8a3ad082497f21160becb9e1406cc13bf8af2542
Samuel Wanjohi <samuel.wanjohi@tracom.co.ke>
2020-04-09 12:27:31 +0300

    Created a notification

--- app/src/main/java/com/example/androids/MainActivity.kt
+++ app/src/main/java/com/example/androids/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.androids
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    val channelId = "com.example.androids"
+    val description = "My notification"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById<Button>(R.id.btn)
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        button.setOnClickListener {
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationChannel =
+                    NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationChannel.enableVibration(true)
+                notificationChannel.lightColor = Color.RED
+                notificationChannel.enableLights(true)
+                notificationManager.createNotificationChannel(notificationChannel)
+
+                builder = Notification.Builder(this, channelId)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("My notification")
+                    .setContentText("Hello World!")
+                     // Set the intent that will fire when the user taps the notification
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+            } else {
+                builder = Notification.Builder(this)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("My notification")
+                    .setContentText("Hello World!")
+                    // Set the intent that will fire when the user taps the notification
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+            }
+            notificationManager.notify(0, builder.build())
+
+        }
     }
 }
